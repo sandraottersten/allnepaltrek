ac9bb616f4c074928997914dd5150b9b9d98831b
sandra <sandra@queenslab.se>
2025-12-06 19:41:35 +0100

    using vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,7 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 
 export default {
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true
-		})
+		adapter: adapter()
 	}
 };
